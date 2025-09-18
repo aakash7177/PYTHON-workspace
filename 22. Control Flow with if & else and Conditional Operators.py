9f585f27371e220a482a090ed6d819357abdd664
aakash7177 <aakash110k@gmail.com>
2025-09-18 23:05:59 +0530

    Add files via upload

---
+++ 22. Control Flow with if & else and Conditional Operators.py
@@ -0,0 +1,36 @@
+# start of notes 1
+"""
+syntex for coonditional statement :-
+if condition :
+    do this
+elif condition :
+    do this
+else :
+    do this
+"""
+# end of notes 1
+
+# start of notes 2
+"""
+In a theme park, the ticket price depends on the visitor’s height. 
+If the height is less than 120 cm, the person is considered a child and pays ₹50. 
+If the height is between 120 cm and 180 cm, they are charged as an adult and pay ₹100.
+For people taller than 180 cm, the park gives a special discount ticket of ₹80.
+This shows how conditional statements (if, elif, and else) help us make multiple decisions 
+based on different conditions, just like in real life.
+
+
+python code :=
+height = int(input("Enter your height in cm: "))
+
+if height < 120:
+    print("You pay ₹50 (child ticket).")
+elif height <= 180:
+    print("You pay ₹100 (adult ticket).")
+else:
+    print("You pay ₹80 (special discount ticket).")
+"""
+
+# end of notes 2
+
+
