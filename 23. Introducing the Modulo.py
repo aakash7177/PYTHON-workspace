37b0a900a6160a2de3f04953c7ac83192b9c8f7a
aakash7177 <aakash110k@gmail.com>
2025-09-19 23:16:50 +0530

    Add files via upload

---
+++ 23. Introducing the Modulo.py
@@ -0,0 +1,29 @@
+# start of notes 1 
+"""
+# modulo operator 
+
+- The modulo operator in Python is written as %.
+- It gives the remainder when one number is divided by another.
+# syntax :
+result = a % b
+- Here, a is the dividend, b is the divisor, and result is the remainder.
+# Examples :
+print(10 % 3)   # 1  (10 divided by 3 = 3 remainder 1)
+print(25 % 7)   # 4  (25 divided by 7 = 3 remainder 4)
+print(8 % 2)    # 0  (8 is divisible by 2)
+print(7 % 8)    # 7  (dividend < divisor, remainder is the dividend)
+
+
+# With negative numbers:
+- Pythons % always gives a remainder with the same sign as the divisor:
+# example2 
+print(10 % -3)   # -2
+print(-10 % 3)   # 2
+
+"""
+# end of notes 1
+
+# Question 1 
+"""
+write a code to check wheather a number is even or odd.
+"""
